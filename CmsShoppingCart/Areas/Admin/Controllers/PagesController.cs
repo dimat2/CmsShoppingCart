155d0185aca1d61e900bca570df7d5143b9959e9
Kiss Péter <kiss.peti86@gmail.com>
2018-07-06 23:13:58 +0200

    Add project files.

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,228 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            List<PageVM> pagesList;
+
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            return View(pagesList);
+        }
+
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            if (!ModelState.IsValid) {
+
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                string slug = "";
+
+                PageDTO dto = new PageDTO();
+
+                dto.Title = model.Title;
+
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "A cím vagy a slug már szerepel az adatbázisban.");
+                }
+
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Új lap hozzáadva.";
+
+            return RedirectToAction("AddPage");
+        }
+
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+
+                if (dto == null)
+                {
+                    return Content("Az oldal nem létezik.");
+                }
+
+                model = new PageVM(dto);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                int id = model.Id;
+
+                string slug = "home";
+
+                PageDTO dto = db.Pages.Find(id);
+
+                dto.Title = model.Title;
+
+                if (model.Slug != "home")
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title) ||
+                    db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "A cím vagy a slug már létezik.");
+                    return View(model);
+                }
+
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Sikeres szerkesztés";
+
+            return RedirectToAction("EditPage");
+        }
+
+        public ActionResult PageDetails(int id)
+        {
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+
+                if (dto == null)
+                {
+                    return Content("A lap nem létezik");
+                }
+
+                model = new PageVM(dto);
+            }
+
+            return View(model);
+        }
+
+        public ActionResult DeletePage(int id)
+        {
+            using (Db db = new Db())
+            {
+                PageDTO dto = db.Pages.Find(id);
+
+                db.Pages.Remove(dto);
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public void RecorderPages(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                int count = 1;
+
+                PageDTO dto;
+
+                foreach (var pageID in id)
+                {
+                    dto = db.Pages.Find(pageID);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+        }
+
+        [HttpGet]
+        public ActionResult EditSidebar()
+        {
+            SidebarVM model;
+
+            using (Db db = new Db())
+            {
+                SidebarDTO dto = db.Sidebar.Find(1);
+
+                model = new SidebarVM(dto);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditSidebar(SidebarVM model)
+        {
+            using (Db db = new Db())
+            {
+                SidebarDTO dto = db.Sidebar.Find(1);
+
+                dto.Body = model.Body;
+
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Szerkesztetted a Sidebar-t";
+
+            return RedirectToAction("EditSidebar");
+        }
+    }
+}
