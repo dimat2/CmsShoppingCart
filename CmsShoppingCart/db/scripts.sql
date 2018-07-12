0d664b40b6b9cc92174701d396bccc9a2bcd5e75
Kiss Péter <kiss.peti86@gmail.com>
2018-07-12 11:56:32 +0200

    Dinamizálás.

--- CmsShoppingCart/db/scripts.sql
+++ CmsShoppingCart/db/scripts.sql
@@ -1,10 +1,12 @@
-﻿CREATE TABLE [dbo].[tblCategories] (
+﻿drop table if exists [dbo].[tblCategories]
+CREATE TABLE [dbo].[tblCategories] (
     [Id]      INT          IDENTITY (1, 1) NOT NULL,
     [Name]    VARCHAR (50) NULL,
     [Slug]    VARCHAR (50) NULL,
     [Sorting] INT          NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+drop table if exists [dbo].[tblPages]
 CREATE TABLE [dbo].[tblPages] (
     [Id]         INT           IDENTITY (1, 1) NOT NULL,
     [Title]      VARCHAR (50)  NULL,
@@ -14,6 +16,9 @@ CREATE TABLE [dbo].[tblPages] (
     [HasSidebar] BIT           NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+INSERT INTO tblPages(Title, Slug, Body, Sorting, HasSidebar) VALUES ('Home', 'home', '<h2>Ez a home.</h2>', 0, 'False')
+
+drop table if exists [dbo].[tblProducts]
 CREATE TABLE [dbo].[tblProducts] (
     [Id]           INT             IDENTITY (1, 1) NOT NULL,
     [Name]         VARCHAR (50)    NULL,
@@ -25,18 +30,23 @@ CREATE TABLE [dbo].[tblProducts] (
     [ImageName]    VARCHAR (100)   NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+drop table if exists [dbo].[tblSidebar]
 CREATE TABLE [dbo].[tblSidebar] (
     [Id]   INT           IDENTITY (1, 1) NOT NULL,
     [Body] VARCHAR (MAX) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+INSERT INTO tblSidebar(Body) VALUES ('<h2>Ez az oldalsáv.</h2>')
 
-
+drop table if exists [dbo].[tblRoles]
 CREATE TABLE [dbo].[tblRoles] (
     [Id]   INT          IDENTITY (1, 1) NOT NULL,
     [Name] VARCHAR (50) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+INSERT INTO tblRoles(Name) VALUES ('Admin'), ('User')
+
+drop table if exists [dbo].[tblUsers]
 CREATE TABLE [dbo].[tblUsers] (
     [Id]           INT          IDENTITY (1, 1) NOT NULL,
     [FirstName]    VARCHAR (50) NULL,
@@ -46,12 +56,12 @@ CREATE TABLE [dbo].[tblUsers] (
     [Password]     VARCHAR (50) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
-CREATE TABLE [dbo].[tblUsers] (
-    [Id]           INT          IDENTITY (1, 1) NOT NULL,
-    [FirstName]    VARCHAR (50) NULL,
-    [LastName]     VARCHAR (50) NULL,
-    [EmailAddress] VARCHAR (50) NULL,
-    [Username]     VARCHAR (50) NULL,
-    [Password]     VARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+INSERT INTO tblUsers(FirstName, LastName, EmailAddress, Username, Password) VALUES ('Péter', 'Kiss', 'kiss.peti86@gmail.com', 'admin', 'pass')
+
+drop table if exists [dbo].[tblUserRoles]
+CREATE TABLE [dbo].[tblUserRoles] (
+    [UserId] INT NOT NULL,
+    [RoleId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([RoleId] ASC, [UserId] ASC)
 );
+INSERT INTO tblUserRoles(UserId, RoleId) VALUES (1, 1)
